fd76710dd8d61ed10fba4810a03edb8c856bdc29
sagarkoshti1990 <sagarkoshti1990@gmail.com>
2022-01-15 09:01:25 +0530

    fix: api call error

--- src/shiksha-os/services/generalServices.js
+++ src/shiksha-os/services/generalServices.js
@@ -9,22 +9,22 @@ const Logout = (e) => {
 
 export async function get(url, headers = {}) {
   return await axios.get(url, {
-    ...headers?.headers,
-    "Access-Control-Allow-Origin": " ",
+    ...headers,
+    headers: { ...headers?.headers, "Access-Control-Allow-Origin": "*" },
   });
 }
 
 export async function post(url, body, headers = {}) {
   return await axios.post(url, body, {
     ...headers,
-    headers: { ...headers?.headers, "Access-Control-Allow-Origin": " " },
+    headers: { ...headers?.headers, "Access-Control-Allow-Origin": "*" },
   });
 }
 
 export async function update(url, body, headers = {}) {
   return await axios.put(url, body, {
     ...headers,
-    headers: { ...headers?.headers, "Access-Control-Allow-Origin": " " },
+    headers: { ...headers?.headers, "Access-Control-Allow-Origin": "*" },
   });
 }
 
